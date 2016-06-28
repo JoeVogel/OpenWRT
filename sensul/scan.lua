a093f5c27d9bf1fd089d27a5096d6f799dfce082
Joe Jonas Vogel <joejvogel@gmail.com>
2016-06-27 23:09:35 -0300

    ADD lua script

---
+++ sensul/scan.lua
@@ -0,0 +1,21 @@
+#!/usr/bin/lua
+
+require("iwinfo")
+
+iface = "wlan0"
+t = iwinfo.type(iface)
+
+while (1=1) do
+	
+	clients = iwinfo[t].assoclist(iface)
+
+	for k,v in pairs(clients) do
+    	os.execute("curl -H \"Content-Type: application/json\" -X POST -d '{\"badge\":\""..k.."\",\"AP\":\"C4:E9:84:6C:9D:EE\",\"signal\":"..v.signal.."}' --interface wlan0 http://192.81.213.239:1880/")
+	end
+
+	os.execute("sleep 3")
+
+end
+
+
+
